304acdeae46d60d645a83c6e7e7e5c3fddc745f9
erikivanov7 <advictoriam@abv.bg>
2022-01-15 18:21:34 +0200

    Updated second solution

---
+++ 3-mysql/1.2-databases-introduction-data-definition-and-datatypes-exercise/02-alter-minions-table.sql
@@ -0,0 +1,10 @@
+ALTER TABLE `towns`
+CHANGE COLUMN `town_id` `id` INT;
+
+ALTER TABLE `minions`
+ADD COLUMN `town_id` INT;
+
+ALTER TABLE `minions`
+ADD CONSTRAINT fk_minions_towns
+FOREIGN KEY `minions`(`town_id`)
+REFERENCES `towns`(`id`) 
