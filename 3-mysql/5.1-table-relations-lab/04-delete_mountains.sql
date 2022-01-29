9be7fc5f1815cf33e1cab73c8f7a9522fd54219b
erikivanov7 <advictoriam@abv.bg>
2022-01-29 17:54:55 +0200

    Added 4th solution

---
+++ 3-mysql/5.1-table-relations-lab/04-delete_mountains.sql
@@ -0,0 +1,16 @@
+DROP TABLE `mountains`, `peaks`;
+
+CREATE TABLE `mountains` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(20)
+);
+
+CREATE TABLE `peaks` (
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(20),
+    `mountain_id` INT,
+    CONSTRAINT `fk_peaks_mountains`
+    FOREIGN KEY (`mountain_id`)
+    REFERENCES `mountains` (`id`)
+    ON DELETE CASCADE
+);
