f615bc605a0a2ad180c2811f26c1f90761b08d86
erikivanov7 <advictoriam@abv.bg>
2022-02-01 23:03:56 +0200

    Added 1st solution

---
+++ 3-mysql/6.2-subqueries-and-joins-exercise/01-employee_address.sql
@@ -0,0 +1,16 @@
+USE `soft_uni`;
+
+SELECT
+`e`.`employee_id`,
+`e`.`job_title`,
+`a`.`address_id`,
+`address_text`
+FROM
+`employees` AS `e`
+JOIN
+`addresses` AS `a`
+ON
+`e`.`address_id` = `a`.`address_id`
+ORDER BY 
+`address_id`
+LIMIT 5;
