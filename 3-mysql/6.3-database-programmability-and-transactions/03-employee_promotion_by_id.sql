8d6a61a6b54be79b74737f275ded28a1c03d7108
erikivanov7 <advictoriam@abv.bg>
2022-02-04 14:35:30 +0200

    Added 3rd solution

---
+++ 3-mysql/6.3-database-programmability-and-transactions/03-employee_promotion_by_id.sql
@@ -0,0 +1,14 @@
+DELIMITER //
+
+CREATE PROCEDURE `usp_raise_salary_by_id`(`id` INT)
+BEGIN
+IF ((SELECT COUNT(*) FROM `employees` WHERE `employee_id` = `id`) = 1)
+THEN
+UPDATE
+`employees`
+SET
+`salary` = `salary` * 1.05
+WHERE
+`employee_id` = `id`;
+END IF;
+END//
