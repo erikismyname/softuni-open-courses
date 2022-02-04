4c5f34d376abc5e364d658d203ff4303881883d7
erikivanov7 <advictoriam@abv.bg>
2022-02-04 13:44:15 +0200

    Added 1st solution

---
+++ 3-mysql/6.3-database-programmability-and-transactions/01-count_employees_by_town.sql
@@ -0,0 +1,17 @@
+DELIMITER //
+
+CREATE FUNCTION `ufn_count_employees_by_town`(`town_name` VARCHAR(20))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+DECLARE `e_count` INT;
+SET `e_count` := (SELECT
+COUNT(*) 
+FROM `employees` AS `e` 
+JOIN `addresses` AS `a` 
+ON `e`.`address_id` = `a`.`address_id` 
+JOIN `towns` AS `t`
+ON `a`.`town_id` = `t`.`town_id` 
+WHERE `t`.`name` = `town_name`);
+RETURN `e_count`;
+END
