148d875cec0eca99f672f4265252db46f59c4773
erikivanov7 <advictoriam@abv.bg>
2022-02-07 12:20:48 +0200

    Added 3rd solution

---
+++ 3-mysql/7.1-database-programmability-and-transactions-exercise/03-town_names_starting_with.sql
@@ -0,0 +1,13 @@
+DELIMITER $$
+
+CREATE PROCEDURE `usp_get_towns_starting_with`(`string` VARCHAR(20))
+BEGIN
+	SELECT `name` AS 'town_name'
+	FROM `towns`
+	WHERE `name` LIKE CONCAT(`string`, '%')
+	ORDER BY `name`;
+END$$
+
+DELIMITER ;
+
+CALL `usp_get_towns_starting_with`('B');
