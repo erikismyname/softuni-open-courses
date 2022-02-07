5cf53669fedaedfb6cda3a290d339fa8107341ad
erikivanov7 <advictoriam@abv.bg>
2022-02-07 16:51:22 +0200

    Added 12th solution

---
+++ 3-mysql/7.1-database-programmability-and-transactions-exercise/12-deposit_money.sql
@@ -0,0 +1,19 @@
+DELIMITER $$
+
+CREATE PROCEDURE `usp_deposit_money`(`account_id` INT, `money_amount` DECIMAL(15, 4))
+BEGIN
+	START TRANSACTION;
+    IF (`money_amount` <= 0) THEN ROLLBACK;
+    ELSE 
+    UPDATE `accounts`
+    SET `balance` = `balance` + `money_amount`
+    WHERE `id` = `account_id`;
+    END IF;
+    SELECT `id`, `account_holder_id`, `balance` 
+    FROM `accounts`
+    WHERE `id` = `account_id`;
+END$$
+
+DELIMITER ;
+
+CALL `usp_deposit_money`(1, -10);
