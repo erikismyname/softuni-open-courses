e285a49991c7796e249e42d5380a76bcce5f5c46
erikivanov7 <advictoriam@abv.bg>
2022-02-08 16:25:47 +0200

    Added 10th solution

---
+++ 3-mysql/8.1-first-exam-preparation/10-find_all_players_that_play_on_stadium.sql
@@ -0,0 +1,16 @@
+DELIMITER $$
+
+CREATE FUNCTION `udf_stadium_players_count`(`stadium_name` VARCHAR(30))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+	RETURN (SELECT COUNT(`p`.`id`)
+	FROM `stadiums` AS `s`
+	LEFT JOIN `teams` AS `t` ON `s`.`id` = `t`.`stadium_id`
+	LEFT JOIN `players` AS `p` ON `t`.`id` = `p`.`team_id`
+	WHERE `s`.`name` = `stadium_name`);
+END$$
+
+DELIMITER ;
+
+SELECT `udf_stadium_players_count`('Jaxworks');
