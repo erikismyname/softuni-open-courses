9290a5aa60ee89fff732a9e866531c2bd356d5bc
erikivanov7 <advictoriam@abv.bg>
2022-02-09 19:18:58 +0200

    Added 6th solution

---
+++ 3-mysql/8.2-second-exam-preparation/06-extract_5_most_commented_photos.sql
@@ -0,0 +1,5 @@
+SELECT `p`.`id`, `p`.`date` AS 'date_and_time', `p`.`description`, COUNT(*) AS 'commentsCount'
+FROM `photos` AS `p`
+LEFT JOIN `comments` AS `c` ON `p`.`id` = `c`.`photo_id`
+GROUP BY `p`.`id`
+ORDER BY `commentsCount` DESC, `p`.`id`;
