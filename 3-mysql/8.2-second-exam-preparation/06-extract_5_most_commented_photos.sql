4fedd8653698a844c78a2ab62b571002cec28f21
erikivanov7 <advictoriam@abv.bg>
2022-02-09 19:21:05 +0200

    Modified 6th solution

--- 3-mysql/8.2-second-exam-preparation/06-extract_5_most_commented_photos.sql
+++ 3-mysql/8.2-second-exam-preparation/06-extract_5_most_commented_photos.sql
@@ -2,4 +2,5 @@ SELECT `p`.`id`, `p`.`date` AS 'date_and_time', `p`.`description`, COUNT(*) AS '
 FROM `photos` AS `p`
 LEFT JOIN `comments` AS `c` ON `p`.`id` = `c`.`photo_id`
 GROUP BY `p`.`id`
-ORDER BY `commentsCount` DESC, `p`.`id`;
+ORDER BY `commentsCount` DESC, `p`.`id`
+LIMIT 5;
