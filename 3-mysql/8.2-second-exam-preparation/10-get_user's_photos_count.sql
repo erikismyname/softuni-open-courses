4a74720d872cb0f24b0835478879a29444f9a86d
erikivanov7 <advictoriam@abv.bg>
2022-02-09 20:09:17 +0200

    Added 10th solution

---
+++ 3-mysql/8.2-second-exam-preparation/10-get_user's_photos_count.sql
@@ -0,0 +1,18 @@
+DELIMITER $$
+
+CREATE FUNCTION `udf_users_photos_count`(`username` VARCHAR(30))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+	RETURN (
+		SELECT COUNT(*)
+		FROM `users` AS `u`
+		JOIN `users_photos` AS `u_p` ON `u`.`id` = `u_p`.`user_id`
+		WHERE `u`.`username` = `username`
+		GROUP BY `u`.`id`
+    );
+END$$
+
+DELIMITER ;
+
+SELECT `udf_users_photos_count`('ssantryd') AS 'photosCount';
