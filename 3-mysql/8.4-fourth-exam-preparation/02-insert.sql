8b411eae9309dbac057ca44f21c23c7fdf3a0f81
erikivanov7 <advictoriam@abv.bg>
2022-02-11 14:11:07 +0200

    Added 2nd solution

---
+++ 3-mysql/8.4-fourth-exam-preparation/02-insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO `games` (`name`, `rating`, `budget`, `team_id`)
+SELECT LOWER(REVERSE(SUBSTRING(`t`.`name`, 2))), `t`.`id`, `t`.`leader_id` * 1000, `t`.`id`
+FROM `teams` AS `t`
+WHERE `t`.`id` BETWEEN 1 AND 9;
