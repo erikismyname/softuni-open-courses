25e798762455a33ac6e6ebfeb5205b9d1194c336
erikivanov7 <advictoriam@abv.bg>
2022-02-12 16:02:11 +0200

    Added 8th solution

---
+++ 3-mysql/8.5-fifth-exam-preparation/08-specific_employee.sql
@@ -0,0 +1,14 @@
+SELECT
+	CONCAT(`e`.`first_name`, ' ', `e`.`last_name`) AS 'Full_name',
+	`s`.`name` AS 'Store_name',
+    `a`.`name` AS 'address',
+    `e`.`salary`
+FROM `employees` AS `e`
+JOIN `stores` AS `s` ON `e`.`store_id` = `s`.`id`
+JOIN `addresses` AS `a` ON `s`.`address_id` = `a`.`id`
+WHERE
+	`e`.`salary` < 4000
+    AND (SELECT `a`.`name`) REGEXP '5'
+    AND CHAR_LENGTH(`s`.`name`) > 8
+    AND RIGHT(`e`.`last_name`, 1) = 'n';
+    
